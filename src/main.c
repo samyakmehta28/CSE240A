41d01c7e9ebac357bcb8f6e4c10a5056791c0cd5
kunind27 <kunind444@gmail.com>
2024-02-28 22:23:51 -0800

    Added custom getline method since UNIX getline not working on Windows

--- src/main.c
+++ src/main.c
@@ -14,6 +14,60 @@ FILE *stream;
 char *buf = NULL;
 size_t len = 0;
 
+// Custom getline function to overcome the error
+size_t getline(char **lineptr, size_t *n, FILE *stream) {
+    char *bufptr = NULL;
+    char *p = bufptr;
+    size_t size;
+    int c;
+
+    if (lineptr == NULL) {
+        return -1;
+    }
+    if (stream == NULL) {
+        return -1;
+    }
+    if (n == NULL) {
+        return -1;
+    }
+    bufptr = *lineptr;
+    size = *n;
+
+    c = fgetc(stream);
+    if (c == EOF) {
+        return -1;
+    }
+    if (bufptr == NULL) {
+        bufptr = malloc(128);
+        if (bufptr == NULL) {
+            return -1;
+        }
+        size = 128;
+    }
+    p = bufptr;
+    while(c != EOF) {
+        if ((p - bufptr) > (size - 1)) {
+            size = size + 128;
+            bufptr = realloc(bufptr, size);
+            if (bufptr == NULL) {
+                return -1;
+            }
+        }
+        *p++ = c;
+        if (c == '\n') {
+            break;
+        }
+        c = fgetc(stream);
+    }
+
+    *p++ = '\0';
+    *lineptr = bufptr;
+    *n = size;
+
+    return p - bufptr - 1;
+}
+
+
 // Print out the Usage information to stderr
 //
 void
@@ -66,6 +120,8 @@ handle_option(char *arg)
 int
 read_branch(uint32_t *pc, uint8_t *outcome)
 {
+  #include <stdio.h> // Include the necessary header file
+
   if (getline(&buf, &len, stream) == -1) {
     return 0;
   }
